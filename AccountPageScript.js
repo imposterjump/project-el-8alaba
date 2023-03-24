3287cddae5fb5909b4b3e4f65d75cbd6b8ef8271
Your Name <yahia1572003@gmail.com>
2023-03-24 04:34:33 +0200

    Update AccountPageScript.js

--- AccountPageScript.js
+++ AccountPageScript.js
@@ -1,19 +1,4 @@
 
-
-  
-            // Add a listener to the form submission to check if passwords are matching
-
-            document.querySelector('form').addEventListener('submit', function(event) {
-            const password = document.querySelector('#password').value;
-            const confirmPassword = document.querySelector('#confirm-password').value;
-
-            // Check if the two passwords match
-            if (password !== confirmPassword) {
-              alert('The two passwords do not match.');
-              event.preventDefault();
-            }
-          });
-
           function openNav() {
             document.getElementById("mySidenav").style.width = "250px";
           }
