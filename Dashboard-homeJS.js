ae73984aace1970e8097248625a39680220e4a03
Abdelrahman27996 <125595297+Abdelrahman27996@users.noreply.github.com>
2023-03-24 16:57:04 +0200

    Analytics and Dash-home JS,CSS files

---
+++ Dashboard-homeJS.js
@@ -0,0 +1,13 @@
+<script>
+function openNav() {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+
+      function closeNav() {
+        document.getElementById("mySidenav").style.width = "0";
+      }
+
+
+
+  
+</script>
