8c09cf5a3664b773f5028e4dc6a9a1a5fc04ae90
Abdelrahman27996 <125595297+Abdelrahman27996@users.noreply.github.com>
2023-05-17 15:34:25 +0300

    donuttt

--- Dashboard-homeJS.js
+++ Dashboard-homeJS.js
@@ -6,3 +6,21 @@ function openNav() {
       function closeNav() {
         document.getElementById("mySidenav").style.width = "0"
       }
+     
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const donutChart = document.getElementById('donutChart').getContext('2d');
+    const doughnutChart = new Chart(donutChart, {
+      type: 'doughnut',
+      data: {
+        labels: ['Product 1', 'prod 2', 'prod 3', 'prod 4', 'prod 5'],
+        datasets: [{
+          data: [20, 20, 20, 20, 20],
+          backgroundColor: [
+            '#FF6384', '#36A2EB', '#FFCE56', '#33FF9E', '#B533FF'
+          ],
+        }]
+      },
+    });
+  });
+
