d9575641ff202281ae127febc2ac1808c08f0658
Abdelrahman27996 <125595297+Abdelrahman27996@users.noreply.github.com>
2023-05-19 19:25:07 +0300

    before end

--- Dashboard-homeJS.js
+++ Dashboard-homeJS.js
@@ -13,9 +13,25 @@ function openNav() {
     const doughnutChart = new Chart(donutChart, {
       type: 'doughnut',
       data: {
-        labels: ['Product 1', 'prod 2', 'prod 3', 'prod 4', 'prod 5'],
+        labels: ['EGY', 'FR', 'GER', 'USA', 'UAE'],
         datasets: [{
           data: [20, 20, 20, 20, 20],
+          backgroundColor: [
+            '#FF6384', '#36A2EB', '#FFCE56', '#33FF9E', '#B533FF'
+          ],
+        }]
+      },
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    const donutChart2 = document.getElementById('donutChart2').getContext('2d');
+    const doughnutChart = new Chart(donutChart2, {
+      type: 'doughnut',
+      data: {
+        labels: ['Egypt', 'France', 'Germany', 'UAE', 'USA'],
+        datasets: [{
+          data: [ 40 , 10 , 5 , 30 , 15 ],
           backgroundColor: [
             '#FF6384', '#36A2EB', '#FFCE56', '#33FF9E', '#B533FF'
           ],
